5022d64147b2bb53dc67a67682b0628077f1c8a1
Jennifer Gomez <jennifergomezbruno@gmail.com>
2019-11-21 14:27:29 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,3 +1,6 @@
 def run_guessing_game
-  random_num = rand(6) + 1 
+  random_num = rand(6) + 1 #generate and store random number
+  puts "Guess a number between 1 and 6!"
+  user_input = gets.chomp #Capture user input from the command line
+  
 end 
